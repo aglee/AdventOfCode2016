50252ae3e3d8e9779095858610a58169a59e63a4
Andy Lee <aglee@mac.com>
2016-12-12 13:34:44 -0500

    Modified the Swift code to look more like the Python code.

--- 12/1.swift
+++ 12/1.swift
@@ -5,49 +5,53 @@ let fileURL = dirURL.appendingPathComponent("input.txt")  // "input.txt" or "tes
 let fileContents = try! String(contentsOf: fileURL)
 let lines = fileContents.components(separatedBy: "\n")
 
-var registers = ["a": 0, "b": 0, "c": 0, "d": 0]
-let instructions = lines.map({ $0.components(separatedBy: " ") })
+func run() {
+	var registers = ["a": 0, "b": 0, "c": 0, "d": 0]
+	registers["c"] = 1  // Comment this out to solve Part 1, add it back to solve Part 2.
 
-func value(_ term: String) -> Int {
-	if let v = registers[term] {
-		return v
-	} else {
-		return Int(term)!
+	func value(_ term: String) -> Int {
+		if let v = registers[term] {
+			return v
+		} else {
+			return Int(term)!
+		}
 	}
+	
+	let instructions = lines.map({ $0.components(separatedBy: " ") })
+
+	NSLog("START")
+	var pc = 0
+	var numCycles = 0
+	while true {
+		numCycles += 1
+		if numCycles % 1000000 == 0 {
+			NSLog("numCycles = %ld", numCycles)
+		}
+		let parts = instructions[pc]
+		var jump = 1
+		switch parts[0] {
+			case "cpy":
+				registers[parts[2]]! = value(parts[1])
+			case "inc":
+				registers[parts[1]]! += 1
+			case "dec":
+				registers[parts[1]]! -= 1
+			case "jnz":
+				if value(parts[1]) != 0 {
+					jump = Int(parts[2])!
+				}
+			default: fatalError("Can't parse instruction '\(instructions[pc])")
+		}
+		pc += jump
+		if pc >= instructions.count {
+			break
+		}
+	}
+	NSLog("DONE")
+	print("registers: \(registers)")
 }
 
-var pc = 0
-func perform(_ parts: [String]) {
-	var jump = 1
-	switch parts[0] {
-		case "cpy":
-			registers[parts[2]]! = value(parts[1])
-		case "inc":
-			registers[parts[1]]! += 1
-		case "dec":
-			registers[parts[1]]! -= 1
-		case "jnz":
-			if value(parts[1]) != 0 {
-				jump = value(parts[2])
-			}
-		default: fatalError("Can't parse instruction '\(instructions[pc])")
-	}
-	pc += jump
-}
+run()
 
-NSLog("START")
-registers["c"] = 1  // Comment this out to solve Part 1, add it back to solve Part 2.
-var numCycles = 0
-while true {
-	numCycles += 1
-	if numCycles % 1000000 == 0 {
-		NSLog("numCycles = %ld", numCycles)
-	}
-	perform(instructions[pc])
-	if pc >= instructions.count {
-		break
-	}
-}
-NSLog("DONE")
-print("registers: \(registers)")
+
 
