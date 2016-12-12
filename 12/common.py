8ceb21299152fb343dcf703ef3794be2f7426103
Andy Lee <aglee@mac.com>
2016-12-12 13:20:50 -0500

    Tweaked the Python code to match the Swift code a little more closely, to see if it would affect the timing significantly.  (It did not.)

--- 12/common.py
+++ 12/common.py
@@ -6,18 +6,11 @@ def readInputLines():
 	filePath = os.path.join(fileDir, "input.txt")  # "input.txt" or "test.txt"
 	return [line.rstrip('\n') for line in open(filePath)]
 
-def loadInstructions():
-	instructions = []
-	for line in readInputLines():
-		parts = line.split(' ')
-		instructions.append((parts[0], parts[1:]))
-	return instructions
-
 def run(registers):
 	def value(term):
 		return registers[term] if (term in 'abcd') else int(term)
 
-	instructions = loadInstructions()
+	instructions = map(lambda x: x.split(' '), readInputLines())
 
 	print("[{}] START".format(datetime.datetime.now()))
 	pc = 0
@@ -26,16 +19,16 @@ def run(registers):
 		numCycles += 1
 		if numCycles % 1000000 == 0:
 			print("[{}] numCycles = {}".format(datetime.datetime.now(), numCycles))
-		(cmd, args) = instructions[pc]
-		if cmd == 'jnz':  # jnz <reg_or_int> <jump>
-			pc += value(args[1]) if value(args[0]) != 0 else 1
+		args = instructions[pc]
+		if args[0] == 'jnz':  # jnz <reg_or_int> <jump>
+			pc += value(args[2]) if value(args[1]) != 0 else 1
 		else:
-			if cmd == 'cpy':  # cpy <reg_or_int> <reg>
-				registers[args[1]] = value(args[0])
-			elif cmd == 'inc':  # inc <reg>
-				registers[args[0]] += 1
-			elif cmd == 'dec':  # dec <reg>
-				registers[args[0]] -= 1
+			if args[0] == 'cpy':  # cpy <reg_or_int> <reg>
+				registers[args[2]] = value(args[1])
+			elif args[0] == 'inc':  # inc <reg>
+				registers[args[1]] += 1
+			elif args[0] == 'dec':  # dec <reg>
+				registers[args[1]] -= 1
 			else:
 				print("Could not parse instruction")
 			pc += 1
